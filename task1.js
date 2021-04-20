8bdee59af8d65e2779acf2a2695f780e59f474ad
Sadanand Akshay Pai <sadypai@gmail.com>
2021-04-20 15:56:51 +0530

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,7 @@
+// Write a function called FooBar that takes integer n and prints all the numbers from 1 upto n. If the number is divisible by 3 then print "Foo", if the number is divisible by 5 then print "Bar" and if the number is divisible by both 3 and 5, print "FooBar". Otherwise, just print the number.
+
+// Ex:
+// Input: n = 15
+// Output: 1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 FooBar
+
+// Solutions
