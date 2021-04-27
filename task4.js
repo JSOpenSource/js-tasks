09bd898cea65baca09bc73ab5faaf19950381767
Sadanand Akshay Pai <sadypai@gmail.com>
2021-04-27 11:11:05 +0530

    Create task4.js

---
+++ task4.js
@@ -0,0 +1,7 @@
+// Write a function which takes an array as input and sorts the array using bubble sort. Also print the array state as output on each pass
+
+// function bubbleSort(array: number[]){
+      // sort logicc
+      // print state of array on each pass
+      // return sortedArray;
+// }
