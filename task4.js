30790ed08c4d423eb4558c6bff009cc8ce142c4c
AnushaHiremath <hiremath.anusha@gmail.com>
2021-05-11 15:27:31 +0530

    Update task4.js

--- task4.js
+++ task4.js
@@ -49,3 +49,18 @@ function sortArrayUsingBubbleSortTechnique(inpArr){
     }
     return(inpArr);
 }
+
+//Anusha
+function bubbleSort (array){
+    for(let i=0;i<array.length-1;i++){
+        for(let j=0;j<array.length-1-i;j++){
+            if(array[j]>array[j+1]){
+                const temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+            }
+        }
+    }
+    return array
+} 
+console.log(bubbleSort([5,3,8,4,6]))
