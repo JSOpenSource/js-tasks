6305fbee9eb311357eb2604260ce3ea98ad2e6cc
Sadanand Akshay Pai <sadypai@gmail.com>
2021-05-28 13:04:37 +0530

    Create task5.js

---
+++ task5.js
@@ -0,0 +1,6 @@
+// Create a single function which can perform sum(a, b, c), sum(a, b)(c), sum(a)(b, c) and sum(a)(b)(c) and returns sum of a, b and c
+// Example
+// sum(2)(4)(6);            // 12
+// sum(3, 2)(5);            // 10
+// sum(4)(-10, -6);         // -12
+// sum(6, -3, 1);           // 4
